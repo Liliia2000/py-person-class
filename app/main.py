b17d2dc241644a535223ee8a8f9ca5cec368ae2f
Liliia Plotnikova <liliiaplotnikova@MacBook-Air-Marcin.local>
2026-03-20 20:49:32 +0100

    new_solution

--- app/main.py
+++ app/main.py
@@ -5,24 +5,19 @@ class Person:
         self.name = name
         self.age = age
         self.people.update({name : self})
-    wife : str
-    husband : str
 
 
 def create_person_list(people: list) -> list:
-    result_list = []
-    for i in people:
-        result_list.append(Person(i["name"], i["age"]))
-    for person in people:
-        for partner in result_list:
-            if partner.name == person["name"]:
-                if "wife" in person.keys() and person["wife"] is not None:
-                    for potential_spouse in result_list:
-                        if person["wife"] == potential_spouse.name:
-                            partner.wife = potential_spouse
-                if "husband" in person.keys():
-                    if person["husband"] is not None:
-                        for potential_spouse in result_list:
-                            if person["husband"] == potential_spouse.name:
-                                partner.husband = potential_spouse
+
+    result_list = [Person(p["name"], p["age"]) for p in people]
+    name_to_person = {p.name: p for p in result_list}
+    for partner in people:
+        person = name_to_person[partner["name"]]
+
+        if partner.get("wife") is not None:
+            person.wife = name_to_person[partner["wife"]]
+
+        if partner.get("husband") is not None:
+            person.husband = name_to_person[partner["husband"]]
+
     return result_list
